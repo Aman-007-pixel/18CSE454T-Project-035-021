8d7dacdf62640d23ce0f7751618513284fc33945
Aman-007-pixel <71084957+Aman-007-pixel@users.noreply.github.com>
2022-05-03 22:07:05 +0530

    Add files via upload

---
+++ framework/cpp_framework.cpp
@@ -0,0 +1,32 @@
+#include "cpp_framework.h"
+
+//------------------------------------------------------------------------------
+// START
+// File    : cpp_framework.h
+// Authorss  : Aman and Kartikeya;  email: ap6044@srmist.edu.in
+// Written : 13 April 2022
+// 
+// Multi-Platform C++ framework
+//
+// Copyright (C) 2022 Aman and Kartikeya.
+// You can use this file only by explicit written approval from Aman and Kartikeya.
+//------------------------------------------------------------------------------
+
+const int CCP::Integer::MIN_VALUE = INT_MIN;
+const int CCP::Integer::MAX_VALUE = INT_MAX;
+const int CCP::Integer::SIZE = 32;
+
+__thread__	CCP::Thread* CCP::_g_tls_current_thread = null;
+
+//------------------------------------------------------------------------------
+// END
+// File    : cpp_framework.h
+// Authorss  : Aman and Kartikeya  email: ap6044@srmist.edu.in
+// Written : 13 April 2022
+// 
+// Multi-Platform C++ framework
+//
+// Copyright (C) 2022 Aman and Kartikeya.
+// You can use this file only by explicit written approval from Aman and Kartikeya.
+//------------------------------------------------------------------------------
+
